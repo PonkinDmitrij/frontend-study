2c2e51d57b91576df66899e37765fcff940903b6
Dmitrij Ponkin <ponkindmitrij@gmail.com>
2022-01-05 17:39:37 +0300

    Add popular

--- botanika/js/index.js
+++ botanika/js/index.js
@@ -22,4 +22,61 @@ const toggleNav = () => {
   navToggle.addEventListener('click', handleNavToggle);
 };
 
+const runSlider = () => {
+  const SLIDER = 'slider';
+  const SLIDER_INNER = `${SLIDER}__inner`;
+  const SLIDE = `${SLIDER}__item`;
+  const CONTROLS = `${SLIDER}__controls`;
+  const BTN_PREV = `${SLIDER}__control--prev`;
+  const BTN_NEXT = `${SLIDER}__control--next`;
+  const slider = document.querySelector(`.${SLIDER}`);
+  const sliderInner = slider.querySelector(`.${SLIDER_INNER}`);
+  const slides = sliderInner.querySelectorAll(`.${SLIDE}`);
+  const controls = slider.querySelector(`.${CONTROLS}`);
+  const btnPrev = controls.querySelector(`.${BTN_PREV}`);
+  const btnNext = controls.querySelector(`.${BTN_NEXT}`);
+  let count = 0;
+  let width;
+
+  if (slides.length <= 1) {
+    controls.hidden = true;
+  }
+
+  const scrollSlider = () => {
+    sliderInner.style.transform = `translate(-${count * width}px)`;
+  };
+
+  const init = () => {
+    width = document.querySelector('.slider').offsetWidth;
+    sliderInner.style.width = `${width * slides.length}px`;
+    slides.forEach((item) => {
+      item.style.width = `${width}px`;
+    });
+    scrollSlider();
+  };
+
+  const blockButtons = () => {
+    btnPrev.disabled = count === 0;
+    btnNext.disabled = count === slides.length - 1;
+  };
+
+  const handleBtnPrev = () => {
+    count--;
+    scrollSlider();
+    blockButtons();
+  };
+
+  const handleBtnNext = () => {
+    count++;
+    scrollSlider();
+    blockButtons();
+  };
+
+  init();
+  window.addEventListener('resize', init);
+  btnPrev.addEventListener('click', handleBtnPrev);
+  btnNext.addEventListener('click', handleBtnNext);
+};
+
 toggleNav();
+runSlider();
