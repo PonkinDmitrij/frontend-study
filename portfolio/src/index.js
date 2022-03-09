d39ea75b80b157467425f5db5ed471e60e03bc17
Dmitrij Ponkin <ponkindmitrij@gmail.com>
2022-03-09 15:12:37 +0300

    refactor: replace the project build system

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,3 +1,4 @@
 // @ts-check
 
+import './scss/style.scss';
 import './js/nav.js';
---
+++ portfolio/webpack.common.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'src/img',
+          to: 'img',
+        },
+      ],
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true, importLoaders: 1, url: false },
+          },
+          { loader: 'sass-loader', options: { sourceMap: true } },
+        ],
+      },
+    ],
+  },
+};
