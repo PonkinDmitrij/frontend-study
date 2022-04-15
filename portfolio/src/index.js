11cddc5fd2c308edd7b968e62e229e1a343d0520
Dmitrij Ponkin <ponkindmitrij@gmail.com>
2022-04-15 13:30:15 +0300

    fix: improve the "switch-lang" implementation

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -3,5 +3,5 @@
 import './scss/style.scss';
 import './js/nav.js';
 import './js/portfolio.js';
-import './js/switch-lang.js';
+import { getTranslate } from './js/switch-lang.js';
 import './js/theme-switch.js';
--- portfolio/src/js/switch-lang.js
+++ portfolio/src/js/switch-lang.js
@@ -6,6 +6,7 @@ const BTN_ACTIVE = `${SWITCH_LANG}__btn--active`;
 const TEXT_NODES = 'data-i18n';
 const switchLang = $(`.${SWITCH_LANG}`);
 const textNodes = document.querySelectorAll(`[${TEXT_NODES}]`);
+const rootElement = document.documentElement;
 
 const toggleClass = (elem, className) => {
   const current = $(`.${className}`);
@@ -13,17 +14,28 @@ const toggleClass = (elem, className) => {
   elem.classList.add(className);
 };
 
+const updateRootAttrLang = (lang) => rootElement.setAttribute('lang', lang);
+
+const updateActiveBtn = (lang) => {
+  const btn = $(`[data-lang=${lang}]`, switchLang);
+  if (btn.classList.contains(BTN_ACTIVE)) {
+    return;
+  }
+  toggleClass(btn, BTN_ACTIVE);
+};
+
 const getTranslate = (lang) => {
   textNodes.forEach((item) => {
     item.textContent = translate[lang][item.dataset.i18n];
   });
+  updateRootAttrLang(lang);
+  updateActiveBtn(lang);
 };
 
 const handleSwitchLang = (event) => {
   const isTagButton = event.target.tagName === 'BUTTON';
   const isButtonActive = event.target.classList.contains(BTN_ACTIVE);
   if (isTagButton && !isButtonActive) {
-    toggleClass(event.target, BTN_ACTIVE);
     getTranslate(event.target.dataset.lang);
   }
 };
